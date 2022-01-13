05fff48c89a56ccb3bb0013075b15cc2500ea9e8
Maria Craciun <georgianamariacraciun@gmail.com>
2022-01-13 11:50:52 +0200

    add BOOST test class + separate class Max in a header file

---
+++ MariaCraciun1_Task1/main.cpp
@@ -0,0 +1,12 @@
+#include <boost/algorithm/string.hpp>
+#include <string>
+#include <iostream>
+#include "Max.h"
+using namespace boost::algorithm;
+
+int main()
+{
+Max nr;
+std::cout<<nr.ComputeMax(5,1,3)<<std::endl;
+
+}
