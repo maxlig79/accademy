bcf88a0896ce22006ef25f97eae3747cf36694a8
silvanadomnica <silvana.domnica@yahoo.com>
2022-01-14 13:09:49 +0200

    Add 5 test cases

--- accademy/silvanadomnica/test.cpp
+++ accademy/silvanadomnica/test.cpp
@@ -2,23 +2,25 @@
 #include <boost/test/included/unit_test.hpp>
 #include "Maxim.hpp"
 
-BOOST_AUTO_TEST_CASE(my_test)
+BOOST_AUTO_TEST_CASE(test1)
 {
-    Maxim max;
-    BOOST_CHECK(max.maximOfThree(1, 2, 3) == 3);
-
-    BOOST_REQUIRE(max.maximOfThree(1, 2, 3) == 3);
-
-    if( max.maximOfThree(1, 2, 3) != 3 )
-      BOOST_ERROR( "Ouch..." );           
-
-    if( max.maximOfThree(1, 2, 3) != 3 )
-      BOOST_FAIL( "Ouch..." );             
-
-    if( max.maximOfThree(1, 2, 3) != 3 ) throw "Ouch..."; 
-
-    BOOST_CHECK_MESSAGE( max.maximOfThree(1, 2, 3) == 3,  
-                         "add(..) result: " << max.maximOfThree(1, 2, 3) );
-
-    BOOST_CHECK_EQUAL(max.maximOfThree(1, 2, 3) , 3);
+    BOOST_CHECK(Maxim::maximOfThree(1, 2, 3) == 3);           
+}
+BOOST_AUTO_TEST_CASE(test2)
+{
+  BOOST_REQUIRE(Maxim::maximOfThree(1, 2, 3) == 3);
+}
+BOOST_AUTO_TEST_CASE(test3)
+{
+   if( Maxim::maximOfThree(1, 2, 3) != 3 )
+      BOOST_ERROR( "Ouch..." );
+}
+BOOST_AUTO_TEST_CASE(test4)
+{
+   BOOST_CHECK_MESSAGE( Maxim::maximOfThree(1, 2, 3) == 3,  
+                         "add(..) result: " << Maxim::maximOfThree(1, 2, 3) );
+}
+BOOST_AUTO_TEST_CASE(test5)
+{
+  BOOST_CHECK_EQUAL(Maxim::maximOfThree(1, 2, 3) , 3);
 }
