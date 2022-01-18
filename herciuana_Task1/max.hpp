cadea9aa8668df7bb10754215ab8e2c7574e2fc5
herciuana <anaherciu99@yahoo.com>
2022-01-18 11:33:54 +0200

    Delet folders

--- herciuana_Task1/max.hpp
+++
@@ -1,9 +0,0 @@
-class maxim
-{
-public:
-    int MaximNumbers(int first, int second, int third)
-    {
-        int max = (first > second) ? ((first > third) ? first : third) : ((second > third) ? second : third);
-        return max;
-    }
-};
