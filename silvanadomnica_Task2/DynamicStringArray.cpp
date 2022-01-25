7f310197c1d3e13772330b71b9cbf6f02cadc82a
silvanadomnica <silvana.domnica@yahoo.com>
2022-01-25 14:13:12 +0200

    Remove multiple return

--- silvanadomnica_Task2/DynamicStringArray.cpp
+++ silvanadomnica_Task2/DynamicStringArray.cpp
@@ -11,7 +11,7 @@ int DynamicStringArray::getSize() const
 }
 void DynamicStringArray::addEntry(const string &newString)
 {
-    string *newArray = new string[size + 1]; 
+    string *newArray = new string[size + 1];
     for (int i = 0; i < size; i++)
     {
         newArray[i] = dynamicArray[i];
@@ -23,26 +23,39 @@ void DynamicStringArray::addEntry(const string &newString)
 }
 bool DynamicStringArray::deleteEntry(const string &newString)
 {
-    for (int i = 0; i < size; i++) {
-        if (dynamicArray[i] == newString) {
-            std::string *newArray = new std::string[size - 1];
-            for (int j = 0; j < i; ++j) {
-                newArray[j] = dynamicArray[j];
-            }
-            for (int j = i+1; j < size; ++j) {
-                newArray[j-1] = dynamicArray[j];
-            }
-            delete[] dynamicArray;
-            dynamicArray = newArray;
-            --size;
-            return true;
+    bool found = false;
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        if (newString == dynamicArray[i])
+        {
+            found = true;
+            break;
         }
     }
-    return false;
+    if (found == true)
+    {
+        std::string *temp = dynamicArray;
+        dynamicArray = new std::string[size - 1];
+        for (int j; j < size; j++)
+        {
+            if (j < i)
+            {
+                dynamicArray[j] = temp[j];
+            }
+            else if (j > i)
+            {
+                dynamicArray[j - 1] = temp[j];
+            }
+        }
+        size--;
+    }
+
+    return found;
 }
 string DynamicStringArray::getEntry(const int index) const
 {
-    if(index >=size)
+    if (index >= size)
     {
         return nullptr;
     }
@@ -53,26 +66,26 @@ string DynamicStringArray::getEntry(const int index) const
 }
 DynamicStringArray::DynamicStringArray(const DynamicStringArray &obj)
 {
-    size=obj.getSize();
-    dynamicArray=new string[size];
-    for(int i=0;i<size;i++)
+    size = obj.getSize();
+    dynamicArray = new string[size];
+    for (int i = 0; i < size; i++)
     {
-        dynamicArray[i]=obj.getEntry(i);
+        dynamicArray[i] = obj.getEntry(i);
     }
 }
 DynamicStringArray::~DynamicStringArray()
 {
     delete[] dynamicArray;
 }
-DynamicStringArray& DynamicStringArray::operator=(const DynamicStringArray &obj)
+DynamicStringArray &DynamicStringArray::operator=(const DynamicStringArray &obj)
 {
-    if(&obj != this)
+    if (&obj != this)
     {
-        size=obj.size;
-        dynamicArray=new string[size];
-        for(int i=0;i<size;i++)
+        size = obj.size;
+        dynamicArray = new string[size];
+        for (int i = 0; i < size; i++)
         {
-            dynamicArray[i]=obj.dynamicArray[i];
+            dynamicArray[i] = obj.dynamicArray[i];
         }
     }
     return *this;
