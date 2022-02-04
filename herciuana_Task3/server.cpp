edf984fe2287f08864cee95f37211c3137005f41
herciuana <anaherciu99@yahoo.com>
2022-02-04 23:25:22 +0200

    Add help,exit,add,delete

--- herciuana_Task3/server.cpp
+++ herciuana_Task3/server.cpp
@@ -14,22 +14,63 @@ int main()
   MessageQueueRequest MQR;
   DynamicStringArray dynamicArray;
   typedef boost::interprocess::allocator<char, boost::interprocess::managed_shared_memory::segment_manager> CharAllocator;
-  typedef boost::interprocess::basic_string<char, std::char_traits<char>, CharAllocator> string;
+  typedef boost::interprocess::basic_string<char, std::char_traits<char>, CharAllocator> stringIpc;
   message_queue::remove(MESSAGE_QUEUE_NAME.c_str());
   shared_memory_object::remove(SHARED_MEMORY_NAME.c_str());
   message_queue mq(create_only, MESSAGE_QUEUE_NAME.c_str(), MAX_MESSAGE_NUMBER, MAX_MESSAGE_SIZE);
   managed_shared_memory msm(create_only, SHARED_MEMORY_NAME.c_str(), SHARED_MEMORY_SIZE);
   message_queue::size_type recv_size;
   unsigned int priority;
-    try
+  auto mutex = msm.find_or_construct<interprocess_mutex>(MUTEX_IPC_NAME.c_str())();
+  auto condition = msm.find_or_construct<interprocess_condition>(COND_IPC_NAME.c_str())();
+  try
   {
-    while (strcmp(MQR.command, "exit") != 0)
+    while (strcmp(MQR.command, "EXIT") != 0)
     {
       mq.receive(&MQR, MAX_MESSAGE_SIZE, recv_size, priority);
-      string *s = msm.find_or_construct<string>(ADD_COMMAND.c_str())("ADD OK", msm.get_segment_manager());
-      cout<<(*s)<<endl;
+      CommandPair commandPair = split_command(MQR.command);
+      scoped_lock<interprocess_mutex> lock(*mutex);
+      switch (commandPair.first)
+      {
+      case CommandIds::HELP:
+      {
+        msm.find_or_construct<stringIpc>(HELP_COMMAND.c_str())("Instructions:", msm.get_segment_manager());
+        break;
+      }
+      case CommandIds::EXIT:
+      {
+        message_queue::remove(MESSAGE_QUEUE_NAME.c_str());
+        shared_memory_object::remove(SHARED_MEMORY_NAME.c_str());
+        break;
+      }
+      case CommandIds::ADD:
+      {
+        dynamicArray.addEntry(commandPair.second);
+        stringIpc *s = msm.find_or_construct<stringIpc>(ADD_COMMAND.c_str())("Add OK", msm.get_segment_manager());
+        cout << (*s) << endl;
+        break;
+      }
+      case CommandIds::DELETE:
+      {
+        if (dynamicArray.deleteEntry(commandPair.second) == true)
+        {
+          msm.construct<stringIpc>(DELETE_COMMAND.c_str())("Delete OK", msm.get_segment_manager());
+        }
+        else
+        {
+          msm.construct<stringIpc>(DELETE_COMMAND.c_str())("Delete NOK", msm.get_segment_manager());
+        }
+        break;
+      }
+      default:
+      {
+        break;
+      }
+      }
+      condition->notify_all();
     }
   }
+
   catch (const std::exception &e)
   {
     std::cerr << e.what() << '\n';
