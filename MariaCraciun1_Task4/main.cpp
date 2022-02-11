02549ff8749582f1f52662e0a0db7263af9684c1
Maria Craciun <georgianamariacraciun@gmail.com>
2022-02-11 11:27:46 +0200

    Add contrast/brightness to image

--- MariaCraciun1_Task4/main.cpp
+++ MariaCraciun1_Task4/main.cpp
@@ -1,16 +1,51 @@
 #include <opencv2/highgui.hpp>
+#include <opencv2/core/matx.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
+const std::string WindowName1 = "Baby Luna";
+const std::string WindowName2 = "Special Luna";
+const std::string Photo = "Luna.jpg";
+const double ScaleFactor = 0.4;
+int Brightness = 20; // values in [0,100]
+double Contrast = 1.2; //values in [0.0, 3.0]
+
 int main( int argc, char** argv )
 {
-    cv::Mat image;
-    image = cv::imread("Luna.jpeg",cv::IMREAD_COLOR);
-    if(! image.data)
+    cv::Mat photoLuna;
+    
+    photoLuna = cv::imread(Photo,cv::IMREAD_COLOR);
+
+    cv::resize(photoLuna, photoLuna, cv::Size(), ScaleFactor, ScaleFactor);
+
+    if(! photoLuna.data)
         {
             std::cout<<"Could not open file" << std::endl;
             return -1;
         }
-    cv::namedWindow("namba image", cv::WINDOW_AUTOSIZE);
-    cv::imshow("namba image", image);
-    cv::waitKey(0);
+    
+    cv::Mat copyPhotoLuna = photoLuna.clone();
+
+    for(int i=0; i<copyPhotoLuna.cols; i++)
+    {
+        for(int j=0; j<copyPhotoLuna.rows; j++)
+        {
+            cv::Vec3b &intensity = copyPhotoLuna.at<cv::Vec3b> (j,i);
+            for(int k=0; k< copyPhotoLuna.channels(); k++)
+            {
+                intensity.val[k]= cv::saturate_cast<uchar>(Contrast * intensity.val[k] + Brightness);
+            }
+        }
+    }
+
+    cv::imshow(WindowName1, photoLuna);
+    cv::moveWindow(WindowName1, 200,0);
+
+    cv::imshow(WindowName2, copyPhotoLuna);
+    cv::moveWindow(WindowName2, 1000,0);
+    
+    cv::waitKey();
+
+    cv::destroyAllWindows;
+
     return 0;
 }
