a647585e24284e8e98ce53683a37835c8cd2ec4e
herciuana <anaherciu99@yahoo.com>
2022-02-11 14:04:37 +0200

    Add contrast and brightness

--- herciuana_Task4/new.cpp
+++ herciuana_Task4/new.cpp
@@ -1,18 +1,18 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/core/types.hpp>
 #include <iostream>
-int main( int argc, char** argv )
+int main(int argc, char **argv)
 {
     cv::Mat image;
-    image = cv::imread("Ana.png",cv::IMREAD_COLOR);
-    if(! image.data)
-        {
-            std::cout<<"Could not open file" << std::endl;
-            return -1;
-        }
+    image = cv::imread("Ana.png", cv::IMREAD_COLOR);
+    if (!image.data)
+    {
+        std::cout << "Could not open file" << std::endl;
+        return -1;
+    }
     cv::Mat new_image = cv::Mat::zeros(image.size(), image.type());
     double contrast = 1.0;
-    int brightness = 0; 
+    int brightness = 0;
     std::cout << "Enter the contrast value [1.0-3.0]: ";
     std::cin >> contrast;
     std::cout << "Enter the brightness value [0-100]: ";
@@ -23,8 +23,8 @@ int main( int argc, char** argv )
         {
             for (int c = 0; c < image.channels(); c++)
             {
-             new_image.at<cv::Vec3b>(i, j)[c] = cv::saturate_cast<uchar>(contrast * image.at<cv::Vec3b>(i, j)[c] + brightness);
-             std::cout<<image.channels()<<std::endl;
+                new_image.at<cv::Vec3b>(i, j)[c] = cv::saturate_cast<uchar>(contrast * image.at<cv::Vec3b>(i, j)[c] + brightness);
+                std::cout << image.channels() << std::endl;
             }
         }
     }
