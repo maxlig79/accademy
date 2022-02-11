cc3bbe4e0f2e6cdda80ee111bfde1c1594d79b68
bogdann98 <your_github_email>
2022-02-11 15:03:18 +0200

    Files for task 2

---
+++ bogdann98_task2/main.cpp
@@ -0,0 +1,14 @@
+#include"DynamicStringArray.h"
+#include<iostream>
+using namespace std;
+int main(){
+
+    DynamicStringArray array;
+    array.addEntry("Romania");
+    array.addEntry("England");
+    array.addEntry("France");
+    array.addEntry("Germany");
+    std::cout<<"The number of elements is "<<array.getSize();
+   
+    return 0;
+}
