b54c098e7145a12da7b4b1a6d7d9fb6d597fc927
herciuana <anaherciu99@yahoo.com>
2022-02-15 22:14:29 +0200

    Add foreach and Timer

--- herciuana_Task4/new.cpp
+++ herciuana_Task4/new.cpp
@@ -1,6 +1,29 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/core/types.hpp>
+#include "opencv2/imgcodecs.hpp"
 #include <iostream>
+#include <execution>
+#include <algorithm>
+#include "new.hpp"
+
+using namespace cv;
+
+cv::Mat BrightnessContrast(const cv::Mat &image, double contrast, double brightness)
+{
+    cv::Mat new_Image = image.clone();
+    std::for_each(
+        new_Image.begin<cv::Vec3b>(),
+        new_Image.end<cv::Vec3b>(),
+        [new_Image, contrast, brightness](cv::Vec3b &img)
+        {
+            for (int k = 0; k < new_Image.channels(); k++)
+            {
+                img[k] = cv::saturate_cast<uchar>(contrast * img[k] + brightness);
+            }
+        });
+    return new_Image;
+}
+
 int main(int argc, char **argv)
 {
     cv::Mat image;
@@ -13,10 +36,11 @@ int main(int argc, char **argv)
     cv::Mat new_image = cv::Mat::zeros(image.size(), image.type());
     double contrast = 1.0;
     int brightness = 0;
-    std::cout << "Enter the contrast value [1.0-3.0]: ";
+    std::cout << "Enter the contrast value [1.0-5.0]: ";
     std::cin >> contrast;
     std::cout << "Enter the brightness value [0-100]: ";
     std::cin >> brightness;
+    Timer t1;
     for (int i = 0; i < image.rows; i++)
     {
         for (int j = 0; j < image.cols; j++)
@@ -24,13 +48,27 @@ int main(int argc, char **argv)
             for (int c = 0; c < image.channels(); c++)
             {
                 new_image.at<cv::Vec3b>(i, j)[c] = cv::saturate_cast<uchar>(contrast * image.at<cv::Vec3b>(i, j)[c] + brightness);
-                std::cout << image.channels() << std::endl;
             }
         }
     }
+    t1.Start("New image:");
+    Timer t2;
+    cv::Mat foreach_image = BrightnessContrast(image, contrast, brightness);
+
+    t2.Start("Foreach: ");
+
     cv::namedWindow("Ana image", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("New image", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Foreach image", cv::WINDOW_AUTOSIZE);
+
     cv::imshow("Ana image", image);
-    cv::imshow("new image", new_image);
+    cv::imshow("New image", new_image);
+    cv::imshow("Foreach image", foreach_image);
+
+    cv::moveWindow("Ana image", 200, 200);
+    cv::moveWindow("New image", 500, 200);
+    cv::moveWindow("Foreach image", 800, 200);
+
     cv::waitKey(0);
     return 0;
 }
