da4465541befe70aa9f26be1476e655d8f160369
vlad-iancu <iancuvladalexandru@gmail.com>
2022-02-21 17:11:15 +0200

    Parallel test

--- vlad_task3/DynamicArrayProxyTest.cpp
+++ vlad_task3/DynamicArrayProxyTest.cpp
@@ -12,6 +12,10 @@
 #include <fstream>
 #include <boost/format.hpp>
 #include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <execution>
+#include <mutex>
 
 using namespace common;
 
@@ -73,47 +77,77 @@ BOOST_FIXTURE_TEST_CASE(DynamicArrayProxy_multiple_clients, Fixture)
     std::ofstream input1(client_input_pipes[0]);
     std::ofstream input2(client_input_pipes[1]);
 
-    for (int i = 0; i < 100; i++)
-    {
-        std::string str1 = (boost::format("add\nstr1%d\n") % i).str();
-        input1 << str1 << std::endl;
-        std::string str2 = (boost::format("add\nstr2%d\n") % i).str();
-        input2 << str2 << std::endl;
-    }
-    for (int i = 0; i < 100; i++)
-    {
-        std::string input = (boost::format("get\n%d") % i).str();
-        input1 << input << std::endl;
-        input2 << input << std::endl;
-    }
+    std::vector<int> vec(100);
+    std::iota(vec.begin(), vec.end(), 0);
+    std::mutex m1, m2;
+    std::for_each(std::execution::par, vec.begin(), vec.end(),
+                  [&](const int i)
+                  {
+                      std::string str1 = (boost::format("add\nstr1_%d\n") % i).str();
+                      m1.lock();
+                      input1 << str1 << std::endl;
+                      m1.unlock();
+                      std::string str2 = (boost::format("add\nstr2_%d\n") % i).str();
+                      m2.lock();
+                      input2 << str2 << std::endl;
+                      m2.unlock();
+                  });
+
+    // std::cout << "Add completed" << std::endl;
+    std::for_each(std::execution::par, vec.begin(), vec.end(),
+                  [&](const int i)
+                  {
+                      std::string input = (boost::format("get\n%d") % i).str();
+                      m1.lock();
+                      input1 << input << std::endl;
+                      m1.unlock();
+                      m2.lock();
+                      input2 << input << std::endl;
+                      m2.unlock();
+                  });
+    // std::cout << "Get completed" << std::endl;
     char line[100] = {0};
     for (int i = 0; i < 100; i++)
     {
         const char msg[] = "String added successfully";
         fgets(line, sizeof(line), client1.get());
+        // std::cout << line;
         BOOST_CHECK(!strcmp(msg, trim(line)));
         memset(line, 0, sizeof(line));
         fgets(line, sizeof(line), client2.get());
+        // std::cout << line;
+        // std::cout << i << std::endl;
         BOOST_CHECK(!strcmp(msg, trim(line)));
         memset(line, 0, sizeof(line));
     }
-
+    // std::cout << "Check adds completed" << std::endl;
+    int res[2][100] = {0};
     for (int i = 0; i < 100; i++)
     {
-        std::string msg;
+        std::string fmt;
 
-        msg = (boost::format("arr[%d]=\"str1%d\"") % i % i).str();
+        // msg = (boost::format("arr[%d]=\"str1%d\"") % i % i).str();
+        fmt = "arr[%d]=\"str%d_%d\"";
+        int idx, client, val;
+
         fgets(line, sizeof(line), client1.get());
+        // std::cout << line;
         trim(line);
-        BOOST_CHECK(!strcmp(msg.c_str(), trim(line)));
+        sscanf(line, fmt.c_str(), &idx, &client, &val);
+        res[client - 1][val] = 1;
         memset(line, 0, sizeof(line));
 
-        msg = (boost::format("arr[%d]=\"str2%d\"") % i % i).str();
         fgets(line, sizeof(line), client2.get());
+        // std::cout << line;
         trim(line);
-        BOOST_CHECK(!strcmp(msg.c_str(), trim(line)));
+        sscanf(line, fmt.c_str(), &idx, &client, &val);
+        res[client - 1][val] = 1;
         memset(line, 0, sizeof(line));
     }
+    BOOST_CHECK(std::all_of(res[0], res[0] + 100, [&](const int i)
+                            { return i == 1; }));
+    BOOST_CHECK(std::all_of(res[1], res[1] + 100, [&](const int i)
+                            { return i == 1; }));
     input1 << "exit" << std::endl;
     input2 << "exit" << std::endl;
     char s[100];
